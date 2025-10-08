2387d3553af3e8ab2e36ab36dd5232a5dda7734a
mindhackdiva <114305417+d0midigi@users.noreply.github.com>
2025-10-08 07:30:08 -0700

    Add files via upload

---
+++ technical-documentation-page/script.js
@@ -0,0 +1,11 @@
+w3CodeColor();
+
+function navOpen() {
+  document.getElementById("navbar").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function navClose() {
+  document.getElementById("navbar").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
