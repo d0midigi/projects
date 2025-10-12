68ae34de495875cb1751c424b7beef39667a6d91
mindhackdiva <114305417+d0midigi@users.noreply.github.com>
2025-10-12 05:44:05 -0700

    Add files via upload

---
+++ blog-design/script.js
@@ -0,0 +1,12 @@
+/* bookmark toggle for articles */
+$(".thisbookmark").click(function(){
+  
+  $(this).toggleClass("fas");
+  
+});
+
+$(".footerbookmark").click(function(){
+  
+  $(this).toggleClass("far");
+  
+});
